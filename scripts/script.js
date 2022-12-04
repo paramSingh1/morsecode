c75de0f078f6ca6dfa0db2d34b2c4f589de8a5a1
paramSingh1 <paramdeep2009@hotmail.com>
2022-12-04 16:19:54 +1100

    branch fix

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-import { translate } from "./translator.js";
-const userInput = document.querySelector(".inputForm__userInput");
-const output = document.querySelector(".inputForm__output");
-
-userInput.addEventListener("input", (event) => {
-  event.preventDefault();
-  output.value = translate(userInput.value);
-});
